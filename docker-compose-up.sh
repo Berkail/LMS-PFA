177af2e6121b1c7659c8816995781c1ab9a4adaa
BERKANI Ilyas <dev.berkail@gmail.com>
2025-03-05 00:54:11 +0000

    added typeORM and established a db connection to postgres

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+if [[ "$1" == "--reset" ]]; then
+  echo "Resetting Docker environment..."
+  docker-compose down -v
+else
+  docker-compose down
+fi
+
 set -a
 source ./back-end/.env
 set +a
+
 docker-compose up --build -d
