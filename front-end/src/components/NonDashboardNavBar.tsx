b6bb7688fc445629e5c148c8b19fb53a06ad0b2f
Yassine Tbibi <tbibi1184@gmail.com>
2025-03-10 14:30:25 +0000

    Adding landing page

--- front-end/src/components/NonDashboardNavBar.tsx
+++ front-end/src/components/NonDashboardNavBar.tsx
@@ -1,3 +1,5 @@
+
+import {Bell, BookOpen} from "lucide-react"
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,14 +7,28 @@ const NonDashboardNavBar = () => {
   return (
     <nav className="nondashboard-navbar">
         <div className="nondashboard-navbar__container">
+           <div className="nondashboard-navbar__search">
             <Link href="/" className="nondashboard-navbar__brand">
                 EHEI-LMS
             </Link>
             <div className='flex item-center gap-4'>
                 <div className='relative group'>
-                    
+                    <Link href="/search" className="nondashboard-navbar__search-input">
+                           <span className="hidden sm:inline">Search Cources</span>
+                           <span className="hidden sm:inline">Search</span>
+                    </Link>
+                    <BookOpen className="nondashboard-navbar__search-icon" size={18}/>
                 </div>
             </div>
+           </div>
+        </div>
+        <div className="nondashboard-navbar__actions">
+            <button className="nondashboard-navbar__notification-button">
+                <span className="nondashboard-navbar__notification-indicator"></span>
+                <Bell className="nondashboard-navbar__notification-icon"></Bell>
+            </button>
+
+
         </div>
     </nav>
   )
