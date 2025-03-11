935d9331c37df2a9634c64f3006090bf4ef5d976
tahay011 <taha.ysf11@gmail.com>
2025-03-11 15:47:48 +0000

    Finish sidebar and navbar; improved UI

--- front-end/src/app/(dashboard)/layout.tsx
+++ front-end/src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AppSidebar from "@/components/AppSidebar";
+import Navbar from "@/components/Navbar";
 import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import { useParams } from "next/navigation";
@@ -8,8 +9,10 @@ import { useParams } from "next/navigation";
 
 
 export default function DashboardLayout({ chidren }: { chidren: React.ReactNode }) {
-    const pathname = useParams();
-
+  
+    const params = useParams();
+    const pathname = params?.pathname || "";
+    const isCoursePage = pathname.includes("courses");
 
   return (
     <SidebarProvider>
@@ -17,7 +20,7 @@ export default function DashboardLayout({ chidren }: { chidren: React.ReactNode 
         <AppSidebar />
         <div className="dashboard__content">
             <div className={cn("dashboard__main")} style={{height: "100vh"}}>
-
+                <Navbar isCoursePage={isCoursePage} />
                 <main className="dashboard__body">{chidren}</main>
             </div>
         </div>
---
+++ front-end/src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+
+import {Bell, BookOpen} from "lucide-react"
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { Button } from "./ui/button"
+import Image from "next/image"
+import { SidebarTrigger } from "./ui/sidebar"
+import { cn } from "@/lib/utils"
+
+const Navbar = ({ isCoursePage }: { isCoursePage: boolean }) => {
+  return (
+    <nav className="dashboard-navbar">
+        <div className="dashboard-navbar__container">
+           <div className="dashboard-navbar__search">
+            <div className='md:hidden'>
+                <SidebarTrigger className="dashboard-navbar__sidebar-trigger" />
+            </div>
+            <div className='flex item-center gap-4'>
+                <div className='relative group'>
+                    <Link href="/search" className={cn("dashboard-navbar__search-input", {
+                        "!bg-customgreys-secondarybg": isCoursePage})}>
+                           <span className="hidden sm:inline">Search Courses</span>
+                           <span className="sm:hidden">Search</span>
+                    </Link>
+                    <BookOpen className="dashboard-navbar__search-icon" size={18}/>
+                </div>
+            </div>
+           </div>
+        </div>
+        <div className="dashboard-navbar__actions">
+            <button className="nondashboard-navbar__notification-button">
+                <span className="nondashboard-navbar__notification-indicator"></span>
+                <Bell className="nondashboard-navbar__notification-icon"></Bell>
+            </button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
