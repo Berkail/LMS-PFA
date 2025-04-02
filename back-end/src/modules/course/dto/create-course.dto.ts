05fdbd6645dfa8218b4a4261ca548f87ba1236e6
BERKANI Ilyas <dev.berkail@gmail.com>
2025-04-02 03:10:20 +0000

    courseModule module init

--- back-end/src/modules/course/dto/create-course.dto.ts
+++ back-end/src/modules/course/dto/create-course.dto.ts
@@ -1,1 +1,5 @@
-export class CreateCourseDto {}
+import { CreateCourseElementDto } from 'src/modules/course-element/dto/create-course-element.dto';
+
+export class CreateCourseDto extends CreateCourseElementDto {
+    
+}
