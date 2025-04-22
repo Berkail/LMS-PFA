8dd0cd873362447ce085799ae6d6a87d07457833
BERKANI Ilyas <dev.berkail@gmail.com>
2025-04-22 01:11:41 +0100

    fixed package import issue where the module is not found

--- front-end/src/components/CustomFormField.tsx
+++ front-end/src/components/CustomFormField.tsx
@@ -24,15 +24,13 @@ import {
 } from "@/components/ui/select";
 import { Switch } from "@/components/ui/switch";
 import { Edit, X, Plus } from "lucide-react";
-import { registerPlugin } from "filepond";
-import { FilePond } from "react-filepond";
+import { FilePond, registerPlugin } from "react-filepond";
 import "filepond/dist/filepond.min.css";
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
-
 interface FormFieldProps {
   name: string;
   label: string;
